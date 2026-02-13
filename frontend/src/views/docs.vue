118003c5b5c5ab810fb42695c2bc30487154cda7
LorenzoSandri <lorenzo.sandri@studenti.unitn.it>
2026-02-13 11:29:55 +0100

    Redoc che mostra le API

---
+++ frontend/src/views/docs.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="docs-container">
+    <redoc spec-url="/oas3.yaml"></redoc>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Docs",
+    mounted() {
+      const script = document.createElement("script")
+      script.src = "https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js"
+      script.async = true
+      document.body.appendChild(script)
+    }
+  }
+</script>
+
+<style scoped>
+  .docs-container {
+    background-color: white;
+    min-height: 100vh;
+  }
+</style>
